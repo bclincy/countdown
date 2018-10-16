92d3f909e5196bb0408c2e3f8dde7b0d603b0a8a
Brian Clincy <bclincy@gmail.com>
2018-10-16 09:04:16 -0400

    update the for ibsporting version

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 const countdown = document.querySelector('.countdown');
-const launchDate = new Date('Jan 1, 2019 00:000:00').getTime();
+const launchDate = new Date('October 26, 2018 00:000:00').getTime();
 var now = new Date().getTime();
-if (launchDate > now ){
-    const intvl = setInterval( ()  => {
+if (launchDate > now) {
+    const intvl = setInterval(() => {
         const now = new Date().getTime();
-        const timeout =  launchDate - now;
+        const timeout = launchDate - now;
         const days = Math.floor(timeout / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeout % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-        const min = Math.floor((timeout % (1000 * 60 * 60))/(1000 * 60));
-        const sec = Math.floor((timeout % (1000 * 60))/1000 );
-        console.log(now);
+        const hours = Math.floor((timeout % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const min = Math.floor((timeout % (1000 * 60 * 60)) / (1000 * 60));
+        const sec = Math.floor((timeout % (1000 * 60)) / 1000);
         countdown.innerHTML = `
         <div>${days}<span>Days</span></div>
         <div>${hours}<span>Hours</span></div>
         <div>${min}<span>Minutes</span></div>
         <div>${sec}<span>Seconds</span></div>
         `;
-    
-    }, 1000);    
+
+    }, 1000);
 } else {
     countdown.style.color = '#CCC';
     countdown.innerHTML = 'Launched!'
